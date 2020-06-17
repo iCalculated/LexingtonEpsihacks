6d070cdac26e7077f049c92ef7e46cb2da31437f
iCalculated <fufa0001@gmail.com>
2020-06-17 16:28:26 -0500

    user page actions

--- blog/app/javascript/packs/application.js
+++ blog/app/javascript/packs/application.js
@@ -3,19 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
-require popper
-require bootstrap
-require jquery3
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require rails-ujs 
+//= require turbolinks 
+//= require jquery3 
+//= require popper 
+//= require bootstrap
