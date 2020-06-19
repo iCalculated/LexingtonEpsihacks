64e949ab5453fe32463f58f153f6e42c1ee1997e
iCalculated <fufa0001@gmail.com>
2020-06-19 11:13:26 -0500

    added search skeleton and cleaned up route config

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+
   devise_for :users
-  get 'welcome/index'
-  resources :articles do
-    resources :comments
-  end
   resources :users, only: [:show, :edit, :update]
   resources :posts, only: [:new, :create, :show, :destroy]
-  root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'search' => 'search#index'
 end
