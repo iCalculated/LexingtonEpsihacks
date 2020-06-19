3ebe3e36ad370bfd8a83fd3c25610b1d72228422
iCalculated <fufa0001@gmail.com>
2020-06-19 11:23:27 -0500

    added search and name

--- blog/app/controllers/search_controller.rb
+++ blog/app/controllers/search_controller.rb
@@ -1,4 +1,10 @@
 class SearchController < ApplicationController
   def index
+    if params[:query].start_with?('#')
+      query = params[:query].gsub('#','')
+      @posts = Post.joins(:tags).where(tags: {name: query})
+    else
+      @posts = Post.where("description LIKE ?","%#{params[:query]}%")
+    end
   end
 end
