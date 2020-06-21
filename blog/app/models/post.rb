97706035d1f1a4557a7f65cee46f3c39893af55e
iCalculated <fufa0001@gmail.com>
2020-06-20 21:57:57 -0500

    fixed posts

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   acts_as_votable
   acts_as_commontable dependent: :destroy
   has_many :post_tags
-	has_many :tags, through: post_tags
+	has_many :tags, through: :post_tags
   after_commit :create_tags, on: :create
   def create_tags
     extract_tags.each do |name|
